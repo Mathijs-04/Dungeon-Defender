d8b3c9f87a93129a99c5e5f62f12f75e14035e4d
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-05-16 17:01:12 +0200

    Game Edited

---
+++ src/js/enemy.js
@@ -0,0 +1,17 @@
+import { Actor } from "excalibur";
+
+export class Enemy extends Actor {
+
+}
+
+export class Skeleton extends Enemy {
+
+}
+
+export class Goblin extends Enemy {
+
+}
+
+export class Bat extends Enemy {
+
+}
