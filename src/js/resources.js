65ad17aaec9cffe98b4d41ab4277e94f06661bb1
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-05-16 17:14:52 +0200

    Game Edited

--- src/js/resources.js
+++ src/js/resources.js
@@ -5,6 +5,11 @@ const Resources = {
     Background: new ImageSource('images/Background.jpg'),
     IdleWizard: new ImageSource('images/IdleWizard.png'),
     RunWizard: new ImageSource('images/RunWizard.png'),
+    IdleSkeleton: new ImageSource('images/IdleSkeleton.png'),
+    RunSkeleton: new ImageSource('images/RunSkeleton.png'),
+    IdleGoblin: new ImageSource('images/IdleGoblin.png'),
+    RunGoblin: new ImageSource('images/RunGoblin.png'),
+    RunBat: new ImageSource('images/RunBat.png'),
 };
 
 const ResourceLoader = new Loader();
